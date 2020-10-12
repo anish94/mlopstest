dcf3b17aaf2d04d6be22d55192372430acd0ced7
Anish Gupta <anishgupta16309@gmail.com>
2020-10-12 15:59:26 -0500

    changing training script

--- training/model_train.py
+++ training/model_train.py
@@ -1,7 +1,7 @@
 import torch 
 import pandas as pd
 import numpy as np
-
+import os
 # Reference:- https://course.fast.ai/videos/?lesson=8
 # importing fastai dependencies
 from fastai.text import TextLMDataBunch, language_model_learner, text_classifier_learner, AWD_LSTM, TextClasDataBunch, DatasetType
@@ -10,8 +10,24 @@ from fastai.callbacks import SaveModelCallback
 # importing preprocessing script
 from clean import preprocess 
 
+from azureml.core import Workspace, Datastore, Dataset
+from azureml.core.run import Run
+from azureml.core.model import Model
+
+run = Run.get_context()
+exp = run.experiment
+ws = run.experiment.workspace
+
+print("Loading training data...")
+datastore = ws.get_default_datastore()
+datastore_paths = [(datastore, 'latest_10052020/latest_10052020.csv')]
+traindata = Dataset.Tabular.from_delimited_files(path=datastore_paths)
+df = traindata.to_pandas_dataframe()
+print("Columns:", df.columns) 
+print("Diabetes data set dimensions : {}".format(df.shape))
+
 # Read training data
-df = pd.read_excel('training-data/08302020.xlsx')
+# df = pd.read_excel('training-data/08302020.xlsx')
 # Preprocessing Subject and OriginalEmailBody (Cleaned result is stored back into OriginalEmailBody)
 clean_df = preprocess(df)
 
@@ -62,4 +78,24 @@ learn.unfreeze()
 learn.fit_one_cycle(15, slice(1e-3/(2.6**4),1e-3), moms=(0.8,0.7), callbacks=[callbacks])
 
 # Save Final model
-learn.export(file = 'nlp-lang-1.pkl')
+# learn.export(file = 'nlp-lang-1.pkl')
+
+# Save model as part of the run history
+print("Exporting the model as pickle file...")
+outputs_folder = './model'
+os.makedirs(outputs_folder, exist_ok=True)
+
+model_filename = "nlp-lang-1.pkl"
+model_path = os.path.join(outputs_folder, model_filename)
+learn.save(model_path)
+
+# upload the model file explicitly into artifacts
+print("Uploading the model into run artifacts...")
+run.upload_file(name="./outputs/models/" + model_filename, path_or_stream=model_path)
+print("Uploaded the model {} to experiment {}".format(model_filename, run.experiment.name))
+dirpath = os.getcwd()
+print(dirpath)
+print("Following files are uploaded ")
+print(run.get_file_names())
+
+run.complete()
